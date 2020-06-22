58fcb4a53f69c32a916dab51b22a99de8e5270b3
Patryk Czarnik <patryk@patrykczarnik.net>
2020-06-22 12:02:19 +0200

    Przykład DOM

---
+++ PC02-Xml/src/sklep/P11_SredniaCenaDom.java
@@ -0,0 +1,59 @@
+package sklep;
+
+import java.util.Objects;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class P11_SredniaCenaDom {
+
+	public static void main(String[] args) {
+		System.out.println("Startujemy..");
+		
+		try {
+			String plik = "pliki/sklep.xml";
+			String kategoria = "herbata";
+			
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+
+			System.out.println("Wczytuję plik...");
+			Document doc = builder.parse(plik);
+			System.out.println("Dokument wczytany: " + doc);
+			
+			// elementem głównym w tym dokumencie jest <sklep>
+			Element elementGlowny = doc.getDocumentElement();
+			System.out.println("element główny: " + elementGlowny);
+			
+			double suma = 0.0;
+			int licznik = 0;
+			
+			NodeList listaTowarow = doc.getElementsByTagName("towar");
+			final int len = listaTowarow.getLength();
+			for(int i = 0; i < len; i++) {
+				Element towar = (Element)listaTowarow.item(i);
+				if(Objects.equals(towar.getAttribute("id-kategorii"), kategoria)) {
+					NodeList znalezioneCeny = towar.getElementsByTagName("cena");
+					if(znalezioneCeny.getLength() > 0) {
+						double cena = Double.parseDouble(znalezioneCeny.item(0).getTextContent());
+						suma += cena;
+						licznik++;
+					}
+				}
+			}
+			
+			double srednia = suma / licznik;
+			System.out.println("Średnia cena: " + srednia);
+			
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
