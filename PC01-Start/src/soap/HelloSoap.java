94d2dcd60c99531e8c69414b93d023e4758157e4
Patryk Czarnik <patryk@patrykczarnik.net>
2020-06-22 10:43:16 +0200

    Parametry SOAP

--- PC01-Start/src/soap/HelloSoap.java
+++ PC01-Start/src/soap/HelloSoap.java
@@ -2,12 +2,15 @@ package soap;
 
 import java.time.LocalTime;
 
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
 @WebService
 public class HelloSoap {
 	
-	public String sayHello(String imie) {
+	@WebResult(name="tekst")
+	public String sayHello(@WebParam(name="imie") String imie) {
 		return "Witaj " + imie + ". Teraz jest godzina " + LocalTime.now();
 	}
 	
