462ee32c870dad202106af60822f5797b01a9b3b
Patryk Czarnik <patryk@patrykczarnik.net>
2020-06-22 12:46:39 +0200

    refaktoryzacja

--- PC02-Xml/src/sklep/P11_SredniaCenaDom.java
+++ PC02-Xml/src/sklep/P11_SredniaCenaDom.java
@@ -25,35 +25,33 @@ public class P11_SredniaCenaDom {
 			Document doc = builder.parse(plik);
 			System.out.println("Dokument wczytany: " + doc);
 			
-			// elementem głównym w tym dokumencie jest <sklep>
-			Element elementGlowny = doc.getDocumentElement();
-			System.out.println("element główny: " + elementGlowny);
-			
-			double suma = 0.0;
-			int licznik = 0;
-			
-			NodeList listaTowarow = doc.getElementsByTagName("towar");
-			final int len = listaTowarow.getLength();
-			for(int i = 0; i < len; i++) {
-				Element towar = (Element)listaTowarow.item(i);
-				if(Objects.equals(towar.getAttribute("id-kategorii"), kategoria)) {
-					NodeList znalezioneCeny = towar.getElementsByTagName("cena");
-					if(znalezioneCeny.getLength() > 0) {
-						double cena = Double.parseDouble(znalezioneCeny.item(0).getTextContent());
-						suma += cena;
-						licznik++;
-					}
-				}
-			}
-			
-			double srednia = suma / licznik;
+			double srednia = sredniaCenaWKategorii(kategoria, doc);
 			System.out.println("Średnia cena: " + srednia);
 			
-		
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
 
+	private static double sredniaCenaWKategorii(String kategoria, Document doc) {
+		double suma = 0.0;
+		int licznik = 0;
+		
+		NodeList listaTowarow = doc.getElementsByTagName("towar");
+		final int len = listaTowarow.getLength();
+		for(int i = 0; i < len; i++) {
+			Element towar = (Element)listaTowarow.item(i);
+			if(Objects.equals(towar.getAttribute("id-kategorii"), kategoria)) {
+				NodeList znalezioneCeny = towar.getElementsByTagName("cena");
+				if(znalezioneCeny.getLength() > 0) {
+					double cena = Double.parseDouble(znalezioneCeny.item(0).getTextContent());
+					suma += cena;
+					licznik++;
+				}
+			}
+		}
+		
+		double srednia = suma / licznik;
+		return srednia;
+	}
 }
