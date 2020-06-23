416a1462335393366856abba3ee367308abdaf15
Patryk Czarnik <patryk@patrykczarnik.net>
2020-06-23 11:42:52 +0200

    Kalkulator v5 - document literal bare

---
+++ PC05-KalkulatorDocumentBare/src/kalkulator/impl/KalkulatorBareImpl.java
@@ -0,0 +1,33 @@
+package kalkulator.impl;
+
+
+import kalkulator.*;
+import javax.jws.WebService;
+
+@WebService(serviceName = "KalkulatorBare", endpointInterface = "kalkulator.KalkulatorBare", targetNamespace = "http://www.example.org/KalkulatorBare/")
+public class KalkulatorBareImpl implements KalkulatorBare {
+	public SubOutput sub(SubInput parameters) {
+		SubOutput wynik = new SubOutput();
+		wynik.setResult(parameters.getArg1() - parameters.getArg2());
+		return wynik;
+	}
+
+	public AddOutput add(AddInput parameters) {
+		AddOutput wynik = new AddOutput();
+		wynik.setResult(parameters.getArg1() + parameters.getArg2());
+		return wynik;
+	}
+
+	public DivOutput div(DivInput parameters) {
+		DivOutput wynik = new DivOutput();
+		wynik.setQuotient(parameters.getArg1() / parameters.getArg2());
+		wynik.setRest(parameters.getArg1() % parameters.getArg2());
+		return wynik;
+	}
+
+	public IncOutput inc(IncInput parameters) {
+		IncOutput wynik = new IncOutput();
+		wynik.setResult(parameters.getArg() + 1);
+		return wynik;
+	}
+}
