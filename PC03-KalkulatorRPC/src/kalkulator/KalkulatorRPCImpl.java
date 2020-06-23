897f776b9fc9a8af202fb7d498661333148fad2c
Patryk Czarnik <patryk@patrykczarnik.net>
2020-06-23 10:09:08 +0200

    Druga wersja kalkulator RPC - nowe operacje i "refaktoryzacja messegów"

--- PC03-KalkulatorRPC/src/kalkulator/KalkulatorRPCImpl.java
+++ PC03-KalkulatorRPC/src/kalkulator/KalkulatorRPCImpl.java
@@ -2,6 +2,7 @@ package kalkulator;
 
 
 import javax.jws.WebService;
+import javax.xml.ws.Holder;
 
 @WebService(serviceName = "KalkulatorRPC",
 	endpointInterface = "kalkulator.KalkulatorRPC",
@@ -21,5 +22,20 @@ public class KalkulatorRPCImpl implements KalkulatorRPC {
 
 	public int inc(int incRequest) {
 		return incRequest+1;
+	}
+
+	public void div(int arg1, int arg2, Holder<Integer> quotient, Holder<Integer> rest) {
+		quotient.value = arg1 / arg2;
+		rest.value = arg1 % arg2;		
+	}
+
+	public float avg(ListaIntow args) {
+		// return args.getArg().stream().mapToInt(x->x).average().getAsDouble();
+		
+		float suma = 0;
+		for(int x : args.getArg()) {
+			suma += x;
+		}
+		return suma / args.getArg().size();
 	}
 }
