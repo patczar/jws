0db02eb27df5f8931741f047e38608587c1a6bfe
Patryk Czarnik <patryk@patrykczarnik.net>
2020-06-29 13:47:39 +0200

    rozdzielenie wersji XML i JSON

--- PC24-OgloszeniaMultiformat/src/main/java/ogloszenia/model/ListaOgloszen.java
+++ PC24-OgloszeniaMultiformat/src/main/java/ogloszenia/model/ListaOgloszen.java
@@ -3,7 +3,12 @@ package ogloszenia.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="ogloszenia")
 public class ListaOgloszen {
+	@XmlElement(name="ogloszenie")
 	public List<Samochodowe> ogloszenia;
 	
 	public ListaOgloszen() {
@@ -13,7 +18,4 @@ public class ListaOgloszen {
 	public ListaOgloszen(List<Samochodowe> ogloszenia) {
 		this.ogloszenia = ogloszenia;
 	}
-	
-	
-	
 }
---
+++ PC24-OgloszeniaMultiformat/src/main/java/ogloszenia/rest/ROgloszeniaXml.java
@@ -0,0 +1,119 @@
+package ogloszenia.rest;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import ogloszenia.baza.DostepDoBazy;
+import ogloszenia.baza.OgloszeniaDAO;
+import ogloszenia.exn.BladBazyDanych;
+import ogloszenia.exn.NieznanyRekord;
+import ogloszenia.model.ListaOgloszen;
+import ogloszenia.model.Samochodowe;
+import ogloszenia.model.Sprzedawca;
+
+@Path("/ogloszenia.xml")
+@Produces("application/xml")
+@Consumes("application/xml")
+public class ROgloszeniaXml {
+
+	@GET
+	public ListaOgloszen odczytajWszystkie() throws BladBazyDanych {
+		try(DostepDoBazy db = new DostepDoBazy()) {
+			OgloszeniaDAO dao = db.ogloszeniaDAO();
+			return new ListaOgloszen(dao.odczytajWszystkie());
+		}
+	}
+	
+	@POST
+	public Samochodowe zapiszOgloszenie(Samochodowe ogloszenie) throws BladBazyDanych {
+		try(DostepDoBazy db = new DostepDoBazy()) {
+			OgloszeniaDAO dao = db.ogloszeniaDAO();
+			dao.zapisz(ogloszenie);
+			// zwracam uzupełniony obiekt - dzięki temu klient dowie się jakie jest jego id
+			return ogloszenie;
+		}
+	}
+	
+	@Path("/{id}")
+	@GET
+	public Samochodowe odczytajJedno(@PathParam("id") int idOgloszenia) throws BladBazyDanych, NieznanyRekord {
+		try(DostepDoBazy db = new DostepDoBazy()) {
+			OgloszeniaDAO dao = db.ogloszeniaDAO();
+			return dao.odczytajWgId(idOgloszenia);
+		}
+	}
+	
+	@Path("/{id}/sprzedawca")
+	@GET
+	public Sprzedawca getSprzedawca(@PathParam("id") int idOgloszenia) throws BladBazyDanych, NieznanyRekord {
+		try(DostepDoBazy db = new DostepDoBazy()) {
+			OgloszeniaDAO dao = db.ogloszeniaDAO();
+			return dao.odczytajWgId(idOgloszenia).getSprzedawca();
+		}
+	}
+	
+	@Path("/{id}/cena")
+	@GET
+	public BigDecimal getCena(@PathParam("id") int idOgloszenia) throws BladBazyDanych, NieznanyRekord {
+		try(DostepDoBazy db = new DostepDoBazy()) {
+			OgloszeniaDAO dao = db.ogloszeniaDAO();
+			return dao.odczytajWgId(idOgloszenia).getCena();
+		}
+	}
+	
+	@Path("/{id}/cena")
+	@PUT
+	// na jedyny parametr pozbawiony adnotacji zostanie wpisana treść (body / entity) przysłana w zapytaniu od klienta
+	public void setCena(@PathParam("id") int idOgloszenia,
+			BigDecimal nowaCena) throws BladBazyDanych, NieznanyRekord {
+		try(DostepDoBazy db = new DostepDoBazy()) {
+			OgloszeniaDAO dao = db.ogloszeniaDAO();
+			Samochodowe ogl = dao.odczytajWgId(idOgloszenia);
+			ogl.setCena(nowaCena);
+			dao.aktualizuj(ogl);
+		}
+	}
+	
+	@Path("/{id}/opis")
+	@GET
+	public String getOpis(@PathParam("id") int idOgloszenia) throws BladBazyDanych, NieznanyRekord {
+		try(DostepDoBazy db = new DostepDoBazy()) {
+			OgloszeniaDAO dao = db.ogloszeniaDAO();
+			Samochodowe ogl = dao.odczytajWgId(idOgloszenia);
+			return ogl.getOpis();
+		}
+	}
+	
+	@Path("/{id}/opis")
+	@PUT
+	public void setOpis(@PathParam("id") int idOgloszenia,
+			String nowyOpis) throws BladBazyDanych, NieznanyRekord {
+		try(DostepDoBazy db = new DostepDoBazy()) {
+			OgloszeniaDAO dao = db.ogloszeniaDAO();
+			Samochodowe ogl = dao.odczytajWgId(idOgloszenia);
+			ogl.setOpis(nowyOpis);
+			dao.aktualizuj(ogl);
+		}
+	}
+	
+	@Path("/{id}/opis")
+	@DELETE
+	public void delOpis(@PathParam("id") int idOgloszenia) throws BladBazyDanych, NieznanyRekord {
+		try(DostepDoBazy db = new DostepDoBazy()) {
+			OgloszeniaDAO dao = db.ogloszeniaDAO();
+			Samochodowe ogl = dao.odczytajWgId(idOgloszenia);
+			ogl.setOpis(null);
+			dao.aktualizuj(ogl);
+		}
+	}
+	
+}
